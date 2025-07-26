4923fd7d9754b656c941e6988b0b55402f1bb6b4
Mitsuhito Fujita <mail@mitsuhito.dev>
2025-07-26 00:26:36 +0000

    fix: "__dirname is not defined in ES module scope"

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -1,6 +1,10 @@
 import path from "node:path";
+import { fileURLToPath } from "node:url";
 import fastifyStatic from "@fastify/static";
 import fastify from "fastify";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const server = fastify();
 
