ac7971e43a43b421ce508f4253b56520d7db25ff
Mitsuhito Fujita <mail@mitsuhito.dev>
2025-08-02 16:23:09 +0000

    fix: change module type

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -1,14 +1,10 @@
 import path from "node:path";
-import { fileURLToPath } from "node:url";
 import fastifyCookie from "@fastify/cookie";
 import fastifyCors from "@fastify/cors";
 import fastifyStatic from "@fastify/static";
 import fastify from "fastify";
 import jwtPlugin from "./plugins/jwt";
 import authRoutes from "./routes/auth";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 const server = fastify({
 	logger: {
